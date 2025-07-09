dd344766cac85ded8ec9a83f1535d9e1da000abf
Kike Rodriguez <enriquemondejar15@gmail.com>
2025-07-09 16:58:38 +0200

    Initial Commit

---
+++ cryptoviewapp/cryptoviewappApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoviewappApp.swift
+//  cryptoviewapp
+//
+//  Created by Kike Rodriguez Vela on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct cryptoviewappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cryptoviewappTests/cryptoviewappTests.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoviewappTests.swift
+//  cryptoviewappTests
+//
+//  Created by Kike Rodriguez Vela on 9/7/25.
+//
+
+import Testing
+@testable import cryptoviewapp
+
+struct cryptoviewappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
